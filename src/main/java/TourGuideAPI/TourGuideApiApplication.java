3fa0452acf5fdaebcdf81ac00291625c36b6dfb3
Justine <justine.herault@live.fr>
2022-04-27 14:47:05 +0200

    Minor fixes

--- src/main/java/TourGuideAPI/TourGuideApiApplication.java
+++ src/main/java/TourGuideAPI/TourGuideApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
-@SpringBootApplication(scanBasePackages = { "gpsUtil" })
+@SpringBootApplication
 public class TourGuideApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/TourGuideAPI/service/TourGuideServiceImpl.java
+++ src/main/java/TourGuideAPI/service/TourGuideServiceImpl.java
@@ -2,7 +2,6 @@ package TourGuideAPI.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import TourGuideAPI.dto.User;
@@ -13,8 +12,7 @@ import gpsUtil.location.VisitedLocation;
 @Service
 public class TourGuideServiceImpl implements TourGuideService {
 
-	@Autowired
-	GpsUtil gpsUtil;
+	private final GpsUtil gpsUtil = new GpsUtil();
 
 	@Override
 	public VisitedLocation getUserLocation(User user) {
