9b2c43e541ebbe2412d9c3e59de92d05fc290526
Justine <justine.herault@live.fr>
2022-04-29 18:53:46 +0200

    Adding all other jar functions

--- src/main/java/TourGuideAPI/service/TourGuideServiceImpl.java
+++ src/main/java/TourGuideAPI/service/TourGuideServiceImpl.java
@@ -5,14 +5,20 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import TourGuideAPI.dto.PriceDTO;
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import rewardCentral.RewardCentral;
+import tripPricer.Provider;
+import tripPricer.TripPricer;
 
 @Service
 public class TourGuideServiceImpl implements TourGuideService {
 
 	private final GpsUtil gpsUtil = new GpsUtil();
+	private final RewardCentral rewardCentral = new RewardCentral();
+	private final TripPricer tripPricer = new TripPricer();
 
 	@Override
 	public VisitedLocation getUserLocation(UUID id) {
@@ -22,6 +28,17 @@ public class TourGuideServiceImpl implements TourGuideService {
 	@Override
 	public List<Attraction> getAttraction() {
 		return gpsUtil.getAttractions();
+	}
+
+	@Override
+	public int getAttractionRewardPoints(UUID attractionID, UUID userID) {
+		return rewardCentral.getAttractionRewardPoints(attractionID, userID);
+	}
+
+	@Override
+	public List<Provider> getPrice(PriceDTO priceDTO) {
+		return tripPricer.getPrice(priceDTO.getTrippricerapikey(), priceDTO.getUserId(), priceDTO.getNumberOfAdults(),
+				priceDTO.getNumberOfChildren(), priceDTO.getTripDuration(), priceDTO.getCumulatativeRewardPoints());
 	}
 
 }
